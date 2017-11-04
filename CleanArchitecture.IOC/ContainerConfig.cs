bd89b105c0d56b06d49d14908dc729bd805c0cfd
Kyle McKee <mckee.kyle@gmail.com>
2017-11-04 02:36:33 -0400

    Dependency inject works!

---
+++ CleanArchitecture.IOC/ContainerConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Mvc;
+using Ninject;
+using Ninject.Web.Mvc;
+
+namespace CleanArchitecture.IOC
+{
+    public static class ContainerConfig
+    {
+        public static void RegisterDependencies(Action<IDependencyResolver> setResolver)
+        {
+            setResolver(new NinjectDependencyResolver(new StandardKernel(
+                new CommonModule()
+            )));
+        }
+    }
+}
--- CleanArchitecture.UI/Global.asax.cs
+++ CleanArchitecture.UI/Global.asax.cs
@@ -2,9 +2,6 @@
 using System.Web.Optimization;
 using System.Web.Routing;
 using CleanArchitecture.IOC;
-using Ninject;
-using Ninject.Web.Common.WebHost;
-using Ninject.Web.Mvc;
 
 namespace CleanArchitecture.UI
 {
@@ -16,7 +13,8 @@ namespace CleanArchitecture.UI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            DependencyResolver.SetResolver(new NinjectDependencyResolver(new ApplicationKernel()));
+            //DependencyResolver.SetResolver(new NinjectDependencyResolver(new ApplicationKernel()));
+            ContainerConfig.RegisterDependencies(DependencyResolver.SetResolver);
         }
     }
 }
